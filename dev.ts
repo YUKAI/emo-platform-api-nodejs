d8297808d7ec3e4781d0298c52624e73f588057a
Keita Arai <chu@ecp.plus>
2022-02-11 14:16:41 +0900

    エラーの場合もdev.tsに記述する

--- dev.ts
+++ dev.ts
@@ -1,5 +1,13 @@
+import type {AxiosError} from 'axios'
 import {AccountRepository} from './dist/index'
 console.log('emo API Client dev client')
 
 console.log('GET /v1/me')
-AccountRepository.getAccountInfo().then(response => console.log(response))
+AccountRepository.getAccountInfo()
+  .then(response => {
+    console.log(response)
+  })
+  .catch((error: AxiosError) => {
+    console.error(`ステータスコード: ${error?.response?.status}`)
+    console.error(error?.response?.data)
+  })
